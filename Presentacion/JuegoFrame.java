55e8300b28e135c7b510e35285aaa0c25d4a76b2
CarlosFranco <cf9220543@gmail.com>
2023-11-21 19:53:19 -0500

    "Se corrigio la interfaz grafica, pero falta agragar mensajes"

--- Presentacion/JuegoFrame.java
+++ Presentacion/JuegoFrame.java
@@ -10,6 +10,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class JuegoFrame extends JFrame {
@@ -19,6 +21,39 @@ public class JuegoFrame extends JFrame {
     private JLabel mensaje;
     private JLabel objetosRestantes;
     private JPanel pnlPrincipal;
+    private JLabel etiquetaHora;
+
+    private JPanel topPanel;
+
+    /*public void RelojDigital() {
+        // Configurar la etiqueta para mostrar la hora
+        etiquetaHora = new JLabel();
+        etiquetaHora.setFont(new Font("Arial", Font.PLAIN, 36));
+        // Agregar la etiqueta a la ventana
+        add(etiquetaHora);
+        setVisible(true);
+
+        actualizarHora(); // Actualizar la hora inicial
+    }
+
+    private void actualizarHora() {
+        // Obtener la hora actual
+        Date horaActual = new Date();
+
+        // Formatear la hora como cadena
+        SimpleDateFormat formatoHora = new SimpleDateFormat("HH:mm:ss");
+        String horaFormateada = formatoHora.format(horaActual);
+
+        // Actualizar el texto de la etiqueta
+        etiquetaHora.setText(horaFormateada);
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        actualizarHora();
+    }*/
 
     Mapa mapa;
     Jugador jugador;
@@ -51,6 +86,7 @@ public class JuegoFrame extends JFrame {
         super("Black Friday"); // Título de la ventana
 
         inicializarJuego();
+        //RelojDigital();
 
         // Obtener el tamaño de la pantalla
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -63,12 +99,17 @@ public class JuegoFrame extends JFrame {
         movimientosLabel = new JLabel("Movimientos: " + jugador.getMovimientos());
         objetosRestantes = new JLabel("");
 
+        mapaPanel.setLayout(new BorderLayout());
 
-        setLayout(new GridLayout(3, 1));
-        add(mapaPanel);
-        add(puntosLabel);
-        add(movimientosLabel);
-        add(objetosRestantes);
+
+        topPanel = new JPanel();
+
+        topPanel.add(puntosLabel);
+        topPanel.add(movimientosLabel);
+        topPanel.add(objetosRestantes);
+
+        add(topPanel, BorderLayout.NORTH);
+        add(mapaPanel, BorderLayout.CENTER);
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null); // Centra la ventana en la pantalla
